b6c8a5237d74c5dcda33f8eaad568bdea0f6e7b4
mypinkshop <mypinkshop1@gmail.com>
2026-05-08 17:19:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import { CartProvider } from './context/CartContext';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
