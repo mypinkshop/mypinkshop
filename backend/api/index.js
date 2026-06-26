a5feb8a9ce541afd660d6627545b775b1de31fdf
mypinkshop <mypinkshop1@gmail.com>
2026-06-27 00:35:35 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -37,6 +37,7 @@ const corsOptions = {
   origin: [
     'https://www.mypinkshop.com',
     'https://mypinkshop.com',
+    'https://api.mypinkshop.com',
     'https://mypinkshop.vercel.app',
     'http://localhost:3000',
     'http://localhost:8081',
@@ -53,6 +54,7 @@ app.use((req, res, next) => {
   const allowedOrigins = [
     'https://www.mypinkshop.com',
     'https://mypinkshop.com',
+    'https://api.mypinkshop.com',
     'https://mypinkshop.vercel.app',
     'http://localhost:3000',
     'http://localhost:8081'
